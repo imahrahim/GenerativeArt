a17d7df912b4d347153a4f6336aa00a95070ef47
imahrahim <115691678+imahrahim@users.noreply.github.com>
2023-04-28 11:09:19 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,8 +5,16 @@ function setup() {
   mgr = new SceneManager();
   mgr.wire();
   mgr.showScene(line0);
+  
+  let button = createButton('Start Audio');
+  button.mousePressed(startAudio);
 }
 
 function draw() {
   mgr.draw();
+}
+
+function startAudio() {
+  // Create the AudioContext after a user gesture
+  context = new AudioContext();
 }
